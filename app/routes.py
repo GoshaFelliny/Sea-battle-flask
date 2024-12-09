64d1549d56ffeabe8a5a357bb65464767dff369d
GoshaFelliny <semen.ss62@gmail.com>
2024-12-09 18:04:29 +0300

    Ограничение на подключения к играм, где уже есть два игрока, а так - же поиск только тех игр где один игрок

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app, socketio
-from flask import render_template
+from flask import render_template, request
 from flask_socketio import emit
 import uuid
 import random
@@ -14,23 +14,39 @@ def index():
 
 @app.route('/game/<game_id>')
 def game(game_id):
-    return render_template('game.html', game_id=game_id)
+    players = rooms.get(game_id, {}).get('players', [])
+    return render_template('game.html', game_id=game_id, players=players)
 
 
 @socketio.on('create_game')
 def create_game():
     game_id = str(uuid.uuid4())
     rooms[game_id] = {'players': [], 'board': []}  # Здесь можно хранить состояние игры
+    rooms[game_id]['players'].append(request.sid)
     emit('game_created', {'game_id': game_id}, broadcast=False)
+    print('Игра создана', game_id)
 
 
 @socketio.on('find_game')
 def find_game():
-    if rooms:
-        # Выбираем случайную игру из существующих
-        game_id = random.choice(list(rooms.keys()))
+    available_games = [game_id for game_id in rooms if len(rooms[game_id]['players']) < 2]
+    if available_games:
+        # Выбираем случайную игру из доступных
+        game_id = random.choice(available_games)
         print(f'Найдена игра: {game_id}')  # Отладочное сообщение
         emit('game_found', {'game_id': game_id}, broadcast=False)
+
+        # Добавляем игрока в комнату
+        rooms[game_id]['players'].append(request.sid)
+        print('Игрок подключен:', request.sid)
+        emit('player_joined', {'player_id': request.sid}, room=game_id)
+
+        # Если это второй игрок, уведомляем обоих игроков
+        if len(rooms[game_id]['players']) == 2:
+            print('Игра может быть запущена')
+            emit('game_start', {'message': 'Игра началась!', 'game_id': game_id}, room=game_id)
     else:
         print('Игры не найдены')  # Отладочное сообщение
         emit('game_found', {'game_id': None}, broadcast=False)  # Если игр нет
+
+
