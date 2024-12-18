deba5c3de388d3275bdd94bf8703ae0d71c52baf
GoshaFelliny <semen.ss62@gmail.com>
2024-12-18 11:53:24 +0300

    Изменен дизайн, сделал drag and drop кораблей для клиентской части

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask_socketio import SocketIO
-
+from app.config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins='*')
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,9 @@
-from app import app, socketio
+from app import app, socketio, db
 from flask import render_template, request
 from flask_socketio import emit
 import uuid
 import random
-
-rooms = {}  #текущие сессии
+from app.model import Player, Room
 
 
 @app.route('/')
@@ -14,22 +13,35 @@ def index():
 
 @app.route('/game/<game_id>')
 def game(game_id):
-    players = rooms.get(game_id, {}).get('players', [])
-    return render_template('game.html', game_id=game_id, players=players)
+    room = Room.query.get(game_id)
+
+    players = room.players if room else []
+    rows = range(1, 11)  # Номера строк от 1 до 10
+    columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']  # Буквы столбцов
+    return render_template('game.html', game_id=game_id, players=players, rows=rows, columns=columns)
 
 
 @socketio.on('create_game')
 def create_game():
     game_id = str(uuid.uuid4())
-    rooms[game_id] = {'players': [], 'board': []}  # Здесь можно хранить состояние игры
-    rooms[game_id]['players'].append(request.sid)
+    new_room = Room(id=game_id, name=f'Game {game_id}')
+    db.session.add(new_room)
+    db.session.commit()
+
+    # Добавляем первого игрока
+    new_player = Player(username=request.sid, sid=request.sid, room_id=new_room.id)
+    db.session.add(new_player)
+    db.session.commit()
+
     emit('game_created', {'game_id': game_id}, broadcast=False)
-    print('Игра создана', game_id)
+    print('Игра создана', game_id, 'игроком', new_player.username)
 
 
 @socketio.on('find_game')
 def find_game():
-    available_games = [game_id for game_id in rooms if len(rooms[game_id]['players']) < 2]
+    available_rooms = Room.query.filter(Room.players.any()).all()
+    available_games = [room.id for room in available_rooms if len(room.players) < 2]
+
     if available_games:
         # Выбираем случайную игру из доступных
         game_id = random.choice(available_games)
@@ -37,16 +49,18 @@ def find_game():
         emit('game_found', {'game_id': game_id}, broadcast=False)
 
         # Добавляем игрока в комнату
-        rooms[game_id]['players'].append(request.sid)
+        room = Room.query.get(game_id)
+        new_player = Player(username=request.sid, sid=request.sid, room_id=room.id)
+        db.session.add(new_player)
+        db.session.commit()
+
         print('Игрок подключен:', request.sid)
         emit('player_joined', {'player_id': request.sid}, room=game_id)
 
         # Если это второй игрок, уведомляем обоих игроков
-        if len(rooms[game_id]['players']) == 2:
+        if len(room.players) == 2:
             print('Игра может быть запущена')
             emit('game_start', {'message': 'Игра началась!', 'game_id': game_id}, room=game_id)
     else:
         print('Игры не найдены')  # Отладочное сообщение
         emit('game_found', {'game_id': None}, broadcast=False)  # Если игр нет
-
-
